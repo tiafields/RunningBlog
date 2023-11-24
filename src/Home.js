5a1c96958576aae3714045b9074b4adf5a971764
Tia Fields <tia.fields226@gmail.com>
2023-11-24 15:28:29 -0500

    adding blog data to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
+    const [blogs, setBlogs] = useState([
+        {title: "first blog", body: "body 1", author: "tia", id: 1},
+        {title: "second blog", body: "body 2", author: "tia", id: 2},
+        {title: "third blog", body: "body 3", author: "mario", id: 3},
+    ]);
+
+    return (
+        <div clasName = "home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p> 
+                </div>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
