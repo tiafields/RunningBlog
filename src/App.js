77f260633152b5c9d714e988ed9c196c2830c3c3
Tia Fields <tia.fields226@gmail.com>
2023-11-28 18:49:29 -0500

    Adding Create.js and set up Router in App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 function App() {
 
   return (
     <Router>
       <div className="App">
           <Navbar />
-            <div className="content">
+            <div className="content">  
               <Switch>
-                <Route path ="/"> 
+                <Route exact path ="/"> 
                   <Home />
+
+                </Route>
+                
+                <Route path ="/create"> 
+                  <Create />
 
                 </Route>
               </Switch>
---
+++ src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+    return ( 
+        <div className="Create">
+            <h2>Add a New Blog</h2>
+            <p> hellow </p>
+        </div>
+     );
+}
+ 
+export default Create;
