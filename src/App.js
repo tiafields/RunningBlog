f3e7f04ee0d0114e29566732df8ca723cba828d4
Tia Fields <tia.fields226@gmail.com>
2023-11-28 19:19:47 -0500

    adding a BlogDetails.js page to create route parameters for individual blogs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 function App() {
 
   return (
@@ -11,15 +12,18 @@ function App() {
           <Navbar />
             <div className="content">  
               <Switch>
+
                 <Route exact path ="/"> 
                   <Home />
-
                 </Route>
                 
                 <Route path ="/create"> 
                   <Create />
-
                 </Route>
+                <Route path ="/blogs/:id">
+                  <BlogDetails />
+                </Route>
+
               </Switch>
             </div>
       </div>
