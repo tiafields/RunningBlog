b7cad3c3a76d50f7a86b7fb5cda2921ac100966d
Tia Fields <tia.fields226@gmail.com>
2023-12-04 23:38:31 -0500

    creating a blog form and adding the css

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,47 @@
+import {useState} from "react"; 
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [date, setDate] = useState('');
+
     return ( 
         <div className="Create">
             <h2>Add a New Blog</h2>
-            <p> hello </p>
+            <form>
+                <label>Blog title: </label>
+                <input  
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}/>
+
+                <label>Blog content:</label> 
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}></textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Tia">Tia</option>
+                    <option value="Mario">Mario</option>
+                </select>
+                    
+
+                <lable>Blog date:</lable>
+                <input      
+                    type="date"
+                    required
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}/>
+        
+                <button>Add Blog</button>
+                
+            </form>
+           
         </div>
      );
 }
